fb36b5c9e808f49b1071d4eb47c27d988ae22b98
Emo2560 <58545434+Emo2560@users.noreply.github.com>
2019-12-26 09:51:11 -0500

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,21 @@
 // Select color input
 // Select size input
 
-// When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function makeGrid(grid) {
+  $("table tr").remove()
+  var cols = $("#inputWidth").val();
+  var rows = $("#inputHeight").val();
+    for (var i = 0; i <= rows; i++) {
+      $("table").append("<tr></tr");
+      for (var j = 0; j <= cols; j++) {
+        $("tr:last").append("<td></td");
+        $("td").attr("class", "cells");
+      }
+    }
+    grid.preventDefault();
+    $(".cells").click(function(event) {
+      var paint = $('#colorPicker').val();
+      $(event.target).css("background-color" , paint");
+    });
 
-function makeGrid() {
-
-// Your code goes here!
-
-}
+  });
